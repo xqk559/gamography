e4e58af76f929fc2b5b20289d06670506d22cf31
keith <xqk559@gmail.com>
2019-12-26 19:26:49 -0500

    List renders properly

--- src/App.js
+++ src/App.js
@@ -4,65 +4,27 @@ import AppFooter from './AppFooter'
 import ItemList from './ItemList'
 import ItemCount from './ItemCount'
 import './App.css';
-import glist from './gameslist.txt';
-// import logo from './logo.svg';
-// import saturnicon from './segasaturnicon.jpg';
+import gamejson from './gameslist2.json'
 
 const axios = require('axios');
 const cheerio = require('cheerio');
+const fuckArrays = [JSON.stringify(gamejson)] ;
+const slagArray = JSON.stringify(gamejson) ;
+const coolArray = slagArray.replace(/\"title\":/g," ") ;
+const spiffyArray = coolArray.replace(/[\{\}\""]/g, "") ;
+const splitArray = spiffyArray.split(",") ;
+const splicedArray = splitArray.splice(-579,570) ;
+const shiftArray= splitArray.shift() ;
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      dumbArray: [6],
-      glist2: glist,
-    };
   }
 
   render() {
-    const url = 'https://segaretro.org/List_of_Saturn_Games_(A-M)';
-
-    axios(url)
-    .then(response => {
-      const html = response.data;
-      const $ = cheerio.load(html)
-      const statsTable = $('tbody > tr');
-      const titles = [];
-
-      statsTable.each(function () {
-        const title = $(this).find('i > a').text();
-        titles.push({
-          title,  
-        });
-      });
-
-      const stringer = JSON.stringify(titles) ;
-      const bestArray = stringer.replace(/"title":/g," ") ;
-
-      const fs = require('fs');
-      const writeStream = fs.createWriteStream('gameslist.txt');
-      const pathName = writeStream.path;
     
-      writeStream.write(bestArray);
+    const allTheThings = splicedArray;
     
-      writeStream.on('finish', () => {
-        console.log(`wrote all the array data to file ${pathName}`);
-      });
-    
-      writeStream.on('error', (err) => {
-        console.error(`There is an error writing the file ${pathName} => ${err}`)
-      });
-    
-      writeStream.end();
-
-      this.setState({dumbArray: bestArray})
-
-      console.log(bestArray)
-    })
-    .catch(console.error);
-
-    const allTheThings = ["2Tax Gold", "3D Baseball", "3D Lemmings", "Test Icicles"];
     return (
       <div>
         <AppJumbotron title="Gamography" />
@@ -75,10 +37,8 @@ class App extends Component {
         <ItemList items={allTheThings} />
         </div>
         <br />
-        <div className="bigmargin">{this.state.dumbArray}</div>
+        <br></br>
         <br />
-        <div>{glist}</div>
-        <div>{this.state.glist2}</div>
         <br />
         <ItemCount count={allTheThings.length} />
         <hr />
